48a07a1d2711cd3cb74c2a0f24469574c8859a0b
playDev11 <play.dev6@gmail.com>
2024-08-27 14:56:19 -0700

    feat: init commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Sidebar from './components/sidebar/sidebar'
+import Main from './components/main/Main'
+
+const App = () => (
+  <>
+    <Sidebar />
+    <Main />
+  </>
+)
+
+export   default App
